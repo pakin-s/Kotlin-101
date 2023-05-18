569eec372ac27d796a950ded7605a57dd1555acf
pakin <pakin.silpasert@gmail.com>
2023-05-18 13:28:20 +0700

    add kotlin-101 homework 1

---
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+fun main () {
+    val number: Int = 2
+    val i: Byte = number.toByte()
+    val s: String = number.toString()
+    val f: Float = number.toFloat()
+
+    val e = 35_000_000
+    val a = 1_000
+
+    println("Number is $number, i is ${number.toByte()}, s is $s")
+    println("e + a = ${e + a}")
+
+    val x = 10
+    var y = 10
+
+    y =  11
+    println("x is $x, y is $y")
+
+    val c = 78
+
+    if (c > 80) {
+        println("A")
+    } else if (c in 70..79) {
+        println("B")
+    } else if (c in 60..69) {
+        println("C")
+    } else {
+        println("F")
+    }
+
+    when(c) {
+        in 80..100 -> println("A")
+        in 70..79 -> println("B")
+        in 60..69 -> println("C")
+        else -> println("F")
+    }
+
+    val z = c downTo 0 step 2
+    println(z)
+
+}
